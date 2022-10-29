7e9d19023f6c386a0b3bfaa61a189e221e9f52af
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-10-30 02:57:27 +0500

    subset

---
+++ src/Questions_Arrays/Subset.java
@@ -0,0 +1,36 @@
+package Questions_Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subset {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        List<List<Integer>> ans = subSet(arr);
+
+        for (List<Integer> list :
+                ans) {
+            System.out.println(list);
+        }
+
+    }
+
+    static List<List<Integer>> subSet(int[] arr) {
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        for (int num :
+                arr) {
+            int n = outer.size();
+
+            for (int i = 0; i < n; i++) {
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+
+
+        return outer;
+    }
+}
