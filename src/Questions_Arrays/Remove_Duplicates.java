eaa04506fe14af3bedeead3e2f14e6174d2e149c
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-11-06 20:07:48 +0500

    Remove

--- src/Questions_Arrays/Remove_Duplicates.java
+++ src/Questions_Arrays/Remove_Duplicates.java
@@ -6,8 +6,8 @@ public class Remove_Duplicates {
     public static void main(String[] args) {
         int[] arr = {0,0,1,1,1,2,2,3,3,4};
 
-       // System.out.println(removeDuplicates(arr));
-        removeDuplicate(arr);
+        System.out.println(removeDuplicates(arr));
+       // removeDuplicate(arr);
     }
     public static void removeDuplicate(int[] arr){
 
