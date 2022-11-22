9bb9c3a7b9f6471bcfd207340bfe36672f64187c
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-11-22 14:02:12 +0500

    Unique Value in Array

--- src/Questions_Arrays/IS_Unique.java
+++ src/Questions_Arrays/IS_Unique.java
@@ -2,14 +2,16 @@ package Questions_Arrays;
 
 public class IS_Unique {
     public static void main(String[] args) {
-        int[] arr = {1,2};
+        int[] arr = {1, 2, 2, 1, 2, 3};
 
         System.out.println(isUnique(arr));
+        System.out.println(uniqueValInArray(arr));
 
     }
-    static boolean isUnique(int[] arr){
+
+    static boolean isUnique(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] == arr[j]) {
                     return false;
                 }
@@ -17,4 +19,26 @@ public class IS_Unique {
         }
         return true;
     }
+
+    static int uniqueValInArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    arr[i] = -1;
+                    arr[j] = -1;
+                }
+            }
+        }
+
+        int ans = -1;
+
+        for (int j : arr) {
+            if (j > 0) {
+                ans = j;
+            }
+        }
+
+        return ans;
+    }
+
 }
