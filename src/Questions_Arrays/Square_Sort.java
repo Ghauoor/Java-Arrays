e263c13e21538d4fd48a2a1404ba4557ff769768
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-11-23 18:46:57 +0500

    sortedSquares

--- src/Questions_Arrays/Square_Sort.java
+++ src/Questions_Arrays/Square_Sort.java
@@ -5,28 +5,26 @@ import java.util.Arrays;
 public class Square_Sort {
     public static void main(String[] args) {
         int[] arr = {-10, -5, -2, 1, 2, 9};
-        System.out.println(Arrays.toString(sortSquare(arr)));
+        System.out.println(Arrays.toString(sortedSquares(arr)));
     }
 
-    static int[] sortSquare(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-
-        int[] ans = new int[arr.length];
-
-        int k = 0;
-
-        while (start <= end) {
+    static public int[] sortedSquares(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+        int start = 0, end = n - 1;
+        for (int p = n - 1; p >= 0; p--) {
             if (Math.abs(arr[start]) > Math.abs(arr[end])) {
-                ans[k++] = arr[start] * arr[start];
+                result[p] = arr[start] * arr[start];
                 start++;
             } else {
-                ans[k++] = arr[end] * arr[end];
+                result[p] = arr[end] * arr[end];
                 end--;
             }
-
         }
-
-        return ans;
+        return result;
     }
 }
+
+
+
+
