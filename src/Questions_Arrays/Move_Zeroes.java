b2ad6f5e8630d97cc49c63231a01a8308f240352
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-01 17:11:34 +0500

    Move Zeros

--- src/Questions_Arrays/Move_Zeroes.java
+++ src/Questions_Arrays/Move_Zeroes.java
@@ -4,29 +4,56 @@ import java.util.Arrays;
 
 public class Move_Zeroes {
     public static void main(String[] args) {
-        int[] arr = {0,1,0,3,12};
+        int[] arr = {0, 1, 0, 3, 12};
+
+        sortZeros(arr);
+        System.out.println(Arrays.toString(arr));
 
         moveZeroes(arr);
         System.out.println(Arrays.toString(arr));
 
 
     }
+
     public static void moveZeroes(int[] arr) {
         int nonZero = 0;
 
-        for (int j = 0; j < arr.length; j++){
-            if(arr[j] != 0){
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] == 0) {
                 swap(arr, j, nonZero);
                 nonZero++;
             }
         }
 
     }
-   static void swap(int[] arr,int i,int j){
+
+    static void sortZeros(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            if (arr[left] == 1 && arr[right] == 0) {
+                swap(arr, left, right);
+                left++;
+                right--;
+            }
+
+            if (arr[left] == 0){
+                left++;
+            } else {
+                right--;
+            }
+
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
 
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
-
     }
+
+
 }
