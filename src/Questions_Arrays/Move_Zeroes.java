b62e49b5714e866706a2ae4e762223dd9d5723c8
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-02 13:23:20 +0500

    Move Zeros

--- src/Questions_Arrays/Move_Zeroes.java
+++ src/Questions_Arrays/Move_Zeroes.java
@@ -4,10 +4,8 @@ import java.util.Arrays;
 
 public class Move_Zeroes {
     public static void main(String[] args) {
-        int[] arr = {0, 1, 0, 3, 12};
+        int[] arr = {0, 3, 6, 8, 0, 3, 12, 0, 0, 3, 2};
 
-        sortZeros(arr);
-        System.out.println(Arrays.toString(arr));
 
         moveZeroes(arr);
         System.out.println(Arrays.toString(arr));
@@ -27,26 +25,6 @@ public class Move_Zeroes {
 
     }
 
-    static void sortZeros(int[] arr) {
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left < right) {
-
-            if (arr[left] == 1 && arr[right] == 0) {
-                swap(arr, left, right);
-                left++;
-                right--;
-            }
-
-            if (arr[left] == 0){
-                left++;
-            } else {
-                right--;
-            }
-
-        }
-    }
 
     static void swap(int[] arr, int i, int j) {
 
