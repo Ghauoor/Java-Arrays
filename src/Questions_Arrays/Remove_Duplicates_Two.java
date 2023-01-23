714dc831c40e9172145bcdc98b2bce25af35682b
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-01-23 21:39:01 +0500

    Remove Duplicates 2

---
+++ src/Questions_Arrays/Remove_Duplicates_Two.java
@@ -0,0 +1,22 @@
+package Questions_Arrays;
+
+public class Remove_Duplicates_Two {
+    public class Solution {
+        public int removeDuplicates(int[] A) {
+            if (A.length <= 2)
+                return A.length;
+            int prev = 1; // point to previous
+            int curr = 2; // point to current
+            while (curr < A.length) {
+                if (A[curr] == A[prev] && A[curr] == A[prev - 1]) {
+                    curr++;
+                } else {
+                    prev++;
+                    A[prev] = A[curr];
+                    curr++;
+                }
+            }
+            return prev + 1;
+        }
+    }
+}
