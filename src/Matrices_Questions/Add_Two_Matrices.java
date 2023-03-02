4c329edbf4a500f09fe8bc1127bebc1fce571918
Ghayoor <ghayoorhussain701@gmail.com>
2023-03-02 19:21:51 +0500

    Add Two Matrices

---
+++ src/Matrices_Questions/Add_Two_Matrices.java
@@ -0,0 +1,17 @@
+package Matrices_Questions;
+
+public class Add_Two_Matrices {
+    void add(int[][] a, int r1, int c1, int[][] b, int r2, int c2) {
+        if (r1 != r2 || c1 != c2) {
+            System.out.println("Wrong Input");
+            return;
+        }
+
+        int[][] sum = new int[r1][c1];
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
+            }
+        }
+    }
+}
