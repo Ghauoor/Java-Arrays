8ff26656330c671fafd2180fd4b109321f7f685d
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-03-27 17:16:50 +0500

    Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.

---
+++ src/Questions_Arrays/Three_Sum_Problem.java
@@ -0,0 +1,32 @@
+package Questions_Arrays;
+
+import java.util.*;
+//https://leetcode.com/problems/3sum/
+public class Three_Sum_Problem {
+    public List<List<Integer>> threeSum(int[] nums) {
+        //Sort the Array
+        Arrays.sort(nums);
+
+        Set<List<Integer>> result = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+}
