56248ceb5c036caab33ccdb29b5c86b6391f9fe4
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-03-30 16:04:25 +0500

    Rotate Image by 90 degree

---
+++ src/Matrices_Questions/Rotate_Image.java
@@ -0,0 +1,28 @@
+package Matrices_Questions;
+//https://leetcode.com/problems/rotate-image/description/
+public class Rotate_Image {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose of Matrix
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
