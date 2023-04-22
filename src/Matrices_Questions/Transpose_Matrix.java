a57d7e24b1ad27465546d34a6c80e0c0d6457316
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-04-22 19:22:54 +0500

    867. Transpose Matrix

---
+++ src/Matrices_Questions/Transpose_Matrix.java
@@ -0,0 +1,13 @@
+package Matrices_Questions;
+//https://leetcode.com/problems/transpose-matrix/description/
+
+public class Transpose_Matrix {
+    public int[][] transpose(int[][] A) {
+        int M = A.length, N = A[0].length;
+        int[][] B = new int[N][M];
+        for (int j = 0; j < N; j++)
+            for (int i = 0; i < M; i++)
+                B[j][i] = A[i][j];
+        return B;
+    }
+}
