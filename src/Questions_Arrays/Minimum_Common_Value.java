c4d7fe4faf8029f2dad2afb4ff4ecb6d21db6f71
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-04-24 11:03:54 +0500

    2540. Minimum Common Value

---
+++ src/Questions_Arrays/Minimum_Common_Value.java
@@ -0,0 +1,19 @@
+package Questions_Arrays;
+
+//https://leetcode.com/problems/minimum-common-value/description/
+public class Minimum_Common_Value {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j])
+                return nums1[i];
+            else if (nums1[i] > nums2[j])
+                j++;
+            else
+                i++;
+        }
+        return -1;
+    }
+}
