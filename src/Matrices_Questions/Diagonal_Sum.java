75759efbd0ef90fcfce0e0242b0417712a835e13
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-05-08 18:00:11 +0500

    1572. Matrix Diagonal Sum

---
+++ src/Matrices_Questions/Diagonal_Sum.java
@@ -0,0 +1,18 @@
+package Matrices_Questions;
+//https://leetcode.com/problems/matrix-diagonal-sum/description/
+public class Diagonal_Sum {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        int n = mat.length;
+
+        for (int i = 0; i < n; i++) {
+            sum += (mat[i][i] + mat[i][n - i - 1]);
+        }
+
+        if (n % 2 == 0) {
+            return sum;
+        } else {
+            return sum - mat[n / 2][n / 2];
+        }
+    }
+}
