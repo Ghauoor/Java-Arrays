38e8f4e9602708d09937926f5ac8a3a9f13641ce
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-06-06 23:42:07 +0500

    1502. Can Make Arithmetic Progression From Sequence

---
+++ src/Questions_Arrays/Can_Make_Arithmetic_Progression_From_Sequence.java
@@ -0,0 +1,30 @@
+package Questions_Arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Can_Make_Arithmetic_Progression_From_Sequence {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Set<Integer> seen = new HashSet<>();
+        int mi = Integer.MAX_VALUE, mx = Integer.MIN_VALUE, n = arr.length;
+        for (int a : arr) {
+            mi = Math.min(mi, a);
+            mx = Math.max(mx, a);
+            seen.add(a);
+        }
+
+        int diff = mx - mi;
+        if (diff % (n - 1) != 0) {
+            return false;
+        }
+
+        diff /= n - 1;
+        while (--n > 0) {
+            if (!seen.contains(mi)) {
+                return false;
+            }
+            mi += diff;
+        }
+        return true;
+    }
+}
